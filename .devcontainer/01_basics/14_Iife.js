627298ec7736cc686ea262ada3bc5517bf4ad906
ak5551 <132918949+ak5551@users.noreply.github.com>
2024-03-07 13:27:15 +0000

    done

--- .devcontainer/01_basics/14_Iife.js
+++ .devcontainer/01_basics/14_Iife.js
@@ -1,1 +1,12 @@
-//Immediately Invoked function expression
+//Immediately Invoked Function Expression
+//Iife = Iife used for immediate function execution and reduce global scope pollution
+
+
+(function chai(){
+    console.log(`DB connected`);
+})();
+
+( (name) => {
+    console.log(`DB connected 2 ${name}`);
+})
+('akash')
